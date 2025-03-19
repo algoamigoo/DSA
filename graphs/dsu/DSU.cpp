3c44fbae4a0c423ad3cad851105c5d507701c62e
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-03-19 10:31:10 +0530

    Update DSU.cpp

--- graphs/dsu/DSU.cpp
+++ graphs/dsu/DSU.cpp
@@ -1,29 +1,26 @@
 
-    vector<int> parent;
-    vector<int> rank;
-    
-int find (int x) {
-        if (x == parent[x]) 
-            return x;
-    
-        return parent[x] = find(parent[x]);
-    }
+vi parent, dsuRank, sizee;
 
-void Union (int x, int y) {
-        int x_parent = find(x);
-        int y_parent = find(y);
-    
-        if (x_parent == y_parent) 
-            return;
-    
-        if(rank[x_parent] > rank[y_parent]) {
-            parent[y_parent] = x_parent;
-        } else if(rank[x_parent] < rank[y_parent]) {
-            parent[x_parent] = y_parent;
-        } else {
-            parent[x_parent] = y_parent;
-            rank[y_parent]++;
-        }
+int find(int x) {
+    if (x != parent[x])
+        parent[x] = find(parent[x]);
+    return parent[x];
 }
 
+void Union(int x, int y) {
+    int px = find(x);
+    int py = find(y);
+    if (px == py) return;
 
+    if (dsuRank[px] < dsuRank[py]) swap(px, py);
+    parent[py] = px;
+    sizee[px] += sizee[py];
+    if (dsuRank[px] == dsuRank[py]) dsuRank[px]++;
+}
+
+in main function
+parent.resize(n + 1);
+    dsuRank.resize(n + 1, 0);
+    sizee.resize(n + 1, 1); 
+
+    fo(i, n + 1) parent[i] = i;
