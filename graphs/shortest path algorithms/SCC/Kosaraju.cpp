c32e0b94d363743c4a6f7381a256ce226ada1f75
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-08-16 08:32:50 +0530

    Create Kosaraju.cpp

---
+++ graphs/shortest path algorithms/SCC/Kosaraju.cpp
@@ -0,0 +1,78 @@
+class Solution
+{
+	public:
+	
+	void dfsFill(int u, vector<vector<int>>& adj, vector<bool> &visited, stack<int>& st) {
+	    visited[u] = true;
+	    
+	    for(int &v : adj[u]) {
+	        
+	        if(!visited[v]) {
+	            dfsFill(v, adj, visited, st);
+	        }
+	        
+	    }
+	    
+	    st.push(u);
+	}
+	
+	void dfsTraverse(int u, vector<vector<int>>& adjReversed, vector<bool> &visited) {
+	    visited[u] = true;
+	    
+	    for(int &v : adjReversed[u]) {
+	        
+	        if(!visited[v]) {
+	            dfsTraverse(v, adjReversed, visited);
+	        }
+	        
+	    }
+
+	}
+	
+	//Function to find number of strongly connected components in the graph.
+    int kosaraju(int V, vector<vector<int>>& adj) {
+        stack<int> st;
+        vector<bool> visited(V, false);
+        
+        for(int i = 0; i<V; i++) {
+            if(!visited[i]) {
+                dfsFill(i, adj, visited, st);
+            }
+        }
+        
+        
+        vector<vector<int>> adjReversed(V);
+        for(int u = 0; u<V; u++) {
+            for(int &v : adj[u]) {
+                
+                //u->v
+                //v->u
+                adjReversed[v].push_back(u);
+                
+            }   
+        }
+        
+        int count = 0;
+        
+        visited = vector<bool>(V, false);
+        while(!st.empty()) {
+            int node = st.top();
+            st.pop();
+            if(!visited[node]) {
+                dfsTraverse(node, adjReversed, visited);
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+};
+
+Perform DFS (dfsFill) on the original graph to push vertices into a stack in order of finishing time.
+
+Build the reversed graph by reversing all edges.
+
+Pop nodes from the stack and run DFS (dfsTraverse) on the reversed graph.
+
+Each DFS run gives one SCC → increment count and finally return i
